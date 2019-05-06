aa4ecdf55247168d7a30a7d81b9d2cdb724a94d8
Diana Y. Velázquez González <47547609+DianaYVg@users.noreply.github.com>
2019-05-06 13:54:29 -0500

    Update enviar.php
    Cambio descripcion del correo

--- assets/php/enviar.php
+++ assets/php/enviar.php
@@ -8,9 +8,10 @@
 
 	$contenido= "Nombre del profesor: ". $nombre."\nUniversidad: ".$universidad."\nCorreo: ".$correo."\nDejo su área de especialización : ".$mensaje;
 
-	$invitado="Gracias por confirmar tu asistencia". $nombre." \nEn breve recibirás detalles del lugar y acceso a <bold> EDUCATHON </bold>. \n \n Se llevará acabo los días 1, 2 y 3 de Julio de 2019.";
+	$invitado="Estimadx". $nombre." 
+	\n\nGracias por tu interés y entusiasmo para participar en Educathon MX, el evento de educación para profesores de IBM México.\nEn breve recibirás todos los detalles sobre el lugar y el acceso, recuerda que el evento se llevará acabo en CDMX los días 10, 11 y 12 de Julio de 2019.\n\n¡Te esperamos!\nIBM Academic Initiative.";
 
-	mail($correo, "Educathon confirmación", $invitado);
+	mail($correo, "¡Gracias por registrarte a Educathon!", $invitado);
 	mail($destino,"Asistencia",$contenido);
 	header("Location:confirmacion.html");
 ?>
